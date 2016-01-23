38f8b8e4665b96a6b4eb78fdac7deed988c3a854
Don Mahurin <@>
2016-01-22 22:12:11 -0800

    Rename 800x480 qml to main.qml

--- demo.sh
+++ demo.sh
@@ -5,7 +5,7 @@ sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan 2>/dev/null
 sudo ip link set up vcan0
 
-canHmi/canHmi --bus-channel-mapping=vcan0=Motor,vcan0=Instrumentation,vcan0=Comfort --kcd-file ./can_definition_sample.kcd --qml-file=samples/QtAutomotiveClusterDemo/qml_800x480.qml &
+canHmi/canHmi --bus-channel-mapping=vcan0=Motor,vcan0=Instrumentation,vcan0=Comfort --kcd-file ./can_definition_sample.kcd --qml-file=samples/QtAutomotiveClusterDemo/main.qml &
 pid1=$!
 sleep 2
 cd samples/simulation ; node simulation.js > /dev/null &
